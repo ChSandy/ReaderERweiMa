da280fd7392f1e0e3233fe05a0c660f89e2d9ba1
ChSandy <1513855069@qq,com$>
2017-01-06 09:46:15 +0800

    基于zxing的一款扫描二维码的应用，首次上传

---
+++ app/src/main/java/com/qf/readererweima/MainActivity.java
@@ -0,0 +1,23 @@
+package com.qf.readererweima;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.zxing.client.android.CaptureActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void open(View view) {
+        Intent intent=new Intent(this, CaptureActivity.class);
+        startActivity(intent);
+
+    }
+}
